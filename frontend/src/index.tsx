f2d116eb5605bbacc5ea96148471e896e52d69e9
Calle <calle.ovinder7@gmail.com>
2024-09-17 19:31:52 +0200

    börjat med routing på ny branch

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState, useEffect} from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,6 +8,45 @@ import reportWebVitals from './reportWebVitals';
 import Signup from './Components/SignIn/signup';
 import NavAppBar from './Components/homeScreen/navBar';
 import SearchBar from './Components/homeScreen/searchBar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import Bookings from './Components/userBookingPage/userBookings'; 
+
+//Används för att hålla koll på globalt tillstånd i individuella komponenter
+export const LoggedinContext = React.createContext<any>(false);    
+
+const Application: React.FC = () => {
+  const [loggedin, setLoggedin] = useState(false);
+
+  useEffect(() => {
+    var sessionLoggedin = window.sessionStorage.getItem("loggedin");
+    setLoggedin(sessionLoggedin === "true"); 
+  }, []);
+
+  useEffect(() => {
+    window.sessionStorage.setItem("loggedin", loggedin.toString()); 
+  }, [loggedin])
+
+  if(!loggedin) {
+    return (
+      <div>
+        <LoggedinContext.Provider value={{loggedin: loggedin, setLoggedin: setLoggedin}}>
+        <Login/>
+        </LoggedinContext.Provider>
+      </div>
+    )
+  }
+  else {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SearchBar/>}/>
+          <Route path="/mybookings" element={<Bookings/>}/>
+          <Route path="navapp" element={<NavAppBar/>}/>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+}; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,11 +54,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/*Ändra på elementet nedan för att rendera din komponent efter att ha importerat den  */}
-    <SearchBar/>
+    {<Application/>}
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); 
