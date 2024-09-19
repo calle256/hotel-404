9e1b13593cebfae368a962ad606e0be882e60762
Calle <calle.ovinder7@gmail.com>
2024-09-19 18:00:56 +0200

    ändrade bookings till table från datagrid

--- frontend/src/Components/userBookingPage/userBookings.tsx
+++ frontend/src/Components/userBookingPage/userBookings.tsx
@@ -5,34 +5,60 @@ import Grid from '@mui/material/Grid2';
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Table from '@mui/material/Table'; 
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
+import TableBody from '@mui/material/TableBody';
+
+
 console.log(bookings); 
 
 
-const columns: GridColDef[] = [
-  {field: 'hotel', headerName: 'Hotel', width: 200}, 
-  {field: 'from_date', headerName: 'Date From', width: 200}, 
-  {field: 'to_date', headerName: 'Date To', width: 200}, 
-  {field: 'cost', headerName: 'Cost', width: 200}, 
-]
 
 const Bookings: React.FC = () => {
+
   return (
     <Grid container
-    spacing={0}
-    alignItems="center"
-    justifyContent="center">
-      <Grid size={12}><h1>Your Bookings</h1></Grid>
-      {//<p>Here you will find all your bookings.</p>
-      }
-      <Grid size={12}>
-        <DataGrid
-        rows={bookings}
-        columns={columns}
-        sx={{border:0}}/>
+    spacing={0}>
+      <Grid size={12}
+      display="flex"
+      alignItems="center"
+      justifyContent="center">
+      <h1>Your Bookings</h1></Grid>
+      <Grid size={12}
+      marginLeft={"100px"}
+      marginRight={"100px"}>
+        <TableContainer component={Paper}>
+          <Table sx={{minWidth: 650}} aria-label="hotel bookings">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Hotel</TableCell>
+                <TableCell align="center">From</TableCell>
+                <TableCell align="center">To</TableCell>
+                <TableCell align="center">Price</TableCell>
+             </TableRow>
+            </TableHead>
+            <TableBody>
+            {bookings.map((booking) => (
+              <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">{booking.hotel}</TableCell>
+              <TableCell align="center">{booking.from_date}</TableCell>
+              <TableCell align="center">{booking.to_date}</TableCell>
+              <TableCell align="center">{booking.cost.toString()}</TableCell>
+            </TableRow>
+            ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
 };
 
 export default Bookings;
+
